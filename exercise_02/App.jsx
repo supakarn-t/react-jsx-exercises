0068cdb1ce68af95bfde450b9e5158c4845fe8d5
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-27 15:21:05 +0700

    finish exercise2

--- exercise_02/App.jsx
+++ exercise_02/App.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
 const App = () => {
-  const temperature = 25;
+	const temperature = 25;
 
-	return <>{/* code here */};</>;
+	function Header() {
+		return (
+			<header>
+				<button>ON/OFF</button>
+				<span>Current Tamperature: {temperature} ํC</span>
+			</header>
+		);
+	}
+
+	function Content() {
+		return (
+			<>
+				<Temperature />
+			</>
+		);
+	}
+
+	function Temperature() {
+		return <p>{temperature}</p>;
+	}
+
+	function Footer() {
+		return (
+			<footer>
+				<button>Down</button>
+				<button>Up</button>
+			</footer>
+		);
+	}
+
+	return (
+		<>
+			<Header />
+			<Content />
+			<Footer />
+		</>
+	);
 };
 
 export default App;
