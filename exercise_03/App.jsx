8ea6b941e140b73a9343a7e53d57b10cadb7eb54
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-28 11:39:40 +0700

    finish exercise3

--- exercise_03/App.jsx
+++ exercise_03/App.jsx
@@ -1,9 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
 	const temperature = 25;
 
-	return <>{/* code here */};</>;
+	return (
+		<>
+			<Header temp={temperature} />
+			<Content temp={temperature} />
+			<Footer />
+		</>
+	);
 };
+
+function Header({ temp }) {
+	return (
+		<header>
+			<button>ON / OFF</button>
+			<span>Current Tamperature: {temp} °C</span>
+		</header>
+	);
+}
+
+function Content({ temp }) {
+	return (
+		<div>
+			<Temperature temp={temp} />
+		</div>
+	);
+}
+
+function Temperature({ temp }) {
+	return <p>{temp} °C</p>;
+}
+
+function Footer() {
+	return (
+		<footer>
+			<button>Down</button>
+			<button>Up</button>
+		</footer>
+	);
+}
 
 export default App;
